7b54afd08ea2c710f9daff3d1f8a32c91fd52438
Glitch (sylveon-website) <none>
2019-01-06 04:42:49 +0000

    🍼🏄 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,17 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+var btnContainer = document.getElementById("navBar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
